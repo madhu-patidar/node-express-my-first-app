50087748efcc5b8dc4243f4236f4fd4f5b6f0553
patidar-madhu <madhusudhan.patidar@swing.live>
2019-06-24 15:06:41 +0530

    Install bootsrap and update the look of page.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
+var passport = require('passport')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('./passport_setup')(passport);
 var app = express();
 
 // view engine setup
@@ -18,6 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret : "our new secret"}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,26 @@
+let models = require('../models');
+
 exports.show_login = function(req, res, next){
     res.render('user/login', {formdata : {}, error : {}} )
 }
 
 exports.show_signup = function(req, res, next){
     res.render('user/login', {formdata : {}, error : {}} )
+}
+
+exports.login = function(req, res, next) {
+    models.Lead.create({
+      email : req.body.email
+    }).then(lead =>{
+      res.redirect('/leads')
+    })
+}
+
+exports.signup = function(req, res, next) {
+    models.User.create({
+      email : req.body.email,
+      password : req.body.password,
+    }).then(lead =>{
+      res.redirect('/leads')
+    })
 }
